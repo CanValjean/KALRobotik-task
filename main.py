2349d356fc57f123ba1b0cee06a9b98186208df9
CanValjean <cankaderliq@gmail.com>
2023-05-13 17:13:44 +0300

    Added a function for calculating the average of the last 20 data points.

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def collect_sensor_data(single_data, data_points):
         data_points.pop(0)
 
     return data_points
+
+
+def calculate_average_distance(data_points):
+    # This function is responsible for calculating the average of the last 20 data points.
+
+    if len(data_points) == 0:
+        return 0
+    # preventing ZeroDivisionError
+    else:
+        average = sum(data_points) / len(data_points)
+    return average
